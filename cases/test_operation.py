90f02e30d7cc3b206448838c0acaa61c60bcfcb6
yxh18 <horch5@163.com>
2021-04-09 10:38:35 +0800

    测试用例实现参数化

---
+++ cases/test_operation.py
@@ -0,0 +1,21 @@
+import pytest
+from testing.counter import Calculator
+
+
+class TestOper:
+    def setup_class(self):
+        print("开始计算")
+        self.calc = Calculator()
+
+    def teardown_class(self):
+        print("计算结束")
+
+    # 参数化加法
+    @pytest.mark.parametrize('param1,param2,expect', [(1, 1, 2), (2, 2, 4), (3, 3, 6)], ids=['case1', 'case2', 'case3'])
+    def test_add(self, param1, param2, expect):
+        assert expect == self.calc.add(param1, param2)
+
+    # 参数化除法
+    @pytest.mark.parametrize('param1,param2,expect', [(10, 5, 2), (5, 5, 1)], ids=['case1', 'case2'])
+    def test_div(self, param1, param2, expect):
+        assert expect == self.calc.div(param1, param2)
---
+++ testing/counter.py
@@ -0,0 +1,11 @@
+# 计算器
+class Calculator:
+    # 相加操作
+    def add(self, a, b):
+        return a + b
+
+    # 相除操作
+    def div(self, a, b):
+        return round(a / b)
+
+# print(TestAdd().div(10,5))
